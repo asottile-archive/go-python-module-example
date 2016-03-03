35cb0cb16dac7bda9a9229628e48d504bf591917
Anthony Sottile <asottile@umich.edu>
2016-03-02 19:55:26 -0800

    Jokes! main() is necessary, also the package name is important

--- step1/sum.go
+++ step1/sum.go
@@ -1,4 +1,5 @@
-package sum
+// asottile: this has to be named main?
+package main
 
 import "C"
 
@@ -11,3 +12,10 @@ import "C"
 func Sum(a, b int) int {
 	return a + b
 }
+
+// asottile: this is required otherwise you get an error
+// go build -buildmode=c-shared -o sum.so sum.go
+// # command-line-arguments
+// runtime.main: call to external function main.main
+// runtime.main: main.main: not defined
+func main() {}
